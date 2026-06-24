ff4dee444ae46b94bc585191ef94626dcba75300
HansonXDi <hansonxd.kawaii@gmail.com>
2026-06-24 00:33:00 -0400

    reinicio

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Script de compilación para las máquinas virtuales.
-# Requiere Go 1.22+ instalado (sudo apt-get install -y golang-go)
-set -e
-echo "[build] Compilando expendedora..."
-GOPROXY=off GOFLAGS="-mod=vendor" go build -o expendedora ./cmd/expendedora/
-echo "[build] Binario 'expendedora' generado correctamente."
--- go.mod
+++
@@ -1,3 +0,0 @@
-module tarea3
-
-go 1.22.2
